fa287d098c2aa6f05d77fb962cbc569d3e2ab34b
Vesal Vojdani <vesal.vojdani@gmail.com>
2015-04-03 10:15:34 +0300

    Update and rename tests in AKTKParserTest.

--- parser/src/test/java/ee/ut/cs/akt/parser/AKTKParserTest.java
+++ parser/src/test/java/ee/ut/cs/akt/parser/AKTKParserTest.java
@@ -9,12 +9,28 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 import static ee.ut.cs.akt.lekser.TokenType.*;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class AKTKParserTest {
 
+
     @Test
-    public void testParse() throws Exception {
+    public void test01_Recognize() throws Exception {
+        check("5+(kala-4)", true);
+        check("5 5", false);
+    }
+
+    private void check(String input, boolean accept) {
+        try {
+            AKTKParser.parse(input);
+            assertTrue(input, accept);
+        } catch (AKTKParseException e) {
+            assertFalse(input, accept);
+        }
+    }
+
+    @Test
+    public void test02_Parse() throws Exception {
         HashMap<String, Integer> env = new HashMap<>();
         env.put("kala", 2);
         ExprNode node = AKTKParser.parse("5+(kala-4)");
@@ -22,13 +38,13 @@ public class AKTKParserTest {
     }
 
     @Test
-    public void testParseException1() throws Exception {
+    public void test03ParseExceptionBoonus1() throws Exception {
         checkException("5+5-+5", 4);
         checkException("5 5", 2);
     }
 
     @Test
-    public void testParseException2() throws Exception {
+    public void test04ParseExceptionBoonus2() throws Exception {
         checkException("5+5-+5", 4, LPAREN, INTEGER, VARIABLE);
         checkException("5 5", 2, PLUS, MINUS, TIMES, DIV, EOF);
     }
